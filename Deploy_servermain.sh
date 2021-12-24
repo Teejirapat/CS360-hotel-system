de68481ffd7e2b4b04d0da55d18b157d6f44c2c9
Teejirapat <64017291+Teejirapat@users.noreply.github.com>
2021-12-24 13:24:04 +0700

    Update Deploy_servermain.sh

--- Deploy_servermain.sh
+++ Deploy_servermain.sh
@@ -2,8 +2,6 @@
 sudo yum -y update
 
 sudo yum install git
-git clone https://github.com/Teejirapat/CS360-hotel-system.git
-cd CS360-hotel-system
 sudo yum install python3
 pip3 install flask pymongo requests bcrypt gevent WSGIServer qrcode libscrc
 sudo amazon-linux-extras install docker -y
